c953144f49c9abb636252ab966590187a5b572ed
zipziip <info@goodkie1.com>
2025-05-12 02:26:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.post('/convert', upload.single('file'), async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server running...');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
